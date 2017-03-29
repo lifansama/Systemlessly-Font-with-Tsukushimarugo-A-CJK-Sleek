b7286944efd6a9ffddb9760c06bbf4c8c81a4abd
HUMORCE <humorce@gmail.com>
2017-03-29 23:02:28 +0800

    reset

--- config.sh
+++ config.sh
@@ -50,7 +50,7 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "           Font Sleek          "
+  ui_print " Font Tsukushimarugo-A & sleek "
   ui_print "*******************************"
 }
 
